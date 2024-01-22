c029e48bf499d7569252e6efb4eb0928f7caca32
serraguti <pacoserranox@gmail.com>
2024-01-22 14:25:07 +0100

    COMMIT

---
+++ AdoNet/Form05DepartamentosEmpleados.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace AdoNet
+{
+    partial class Form05DepartamentosEmpleados
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            lstDepartamentos = new ListBox();
+            label2 = new Label();
+            lstEmpleados = new ListBox();
+            label3 = new Label();
+            txtOficio = new TextBox();
+            label4 = new Label();
+            txtSalario = new TextBox();
+            btnModificarDatos = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(28, 23);
+            label1.Name = "label1";
+            label1.Size = new Size(178, 32);
+            label1.TabIndex = 0;
+            label1.Text = "Departamentos";
+            // 
+            // lstDepartamentos
+            // 
+            lstDepartamentos.FormattingEnabled = true;
+            lstDepartamentos.ItemHeight = 32;
+            lstDepartamentos.Location = new Point(33, 59);
+            lstDepartamentos.Name = "lstDepartamentos";
+            lstDepartamentos.Size = new Size(271, 228);
+            lstDepartamentos.TabIndex = 1;
+            lstDepartamentos.SelectedIndexChanged += lstDepartamentos_SelectedIndexChanged;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(331, 22);
+            label2.Name = "label2";
+            label2.Size = new Size(130, 32);
+            label2.TabIndex = 2;
+            label2.Text = "Empleados";
+            // 
+            // lstEmpleados
+            // 
+            lstEmpleados.FormattingEnabled = true;
+            lstEmpleados.ItemHeight = 32;
+            lstEmpleados.Location = new Point(338, 59);
+            lstEmpleados.Name = "lstEmpleados";
+            lstEmpleados.Size = new Size(258, 324);
+            lstEmpleados.TabIndex = 3;
+            lstEmpleados.SelectedIndexChanged += lstEmpleados_SelectedIndexChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(623, 24);
+            label3.Name = "label3";
+            label3.Size = new Size(77, 32);
+            label3.TabIndex = 4;
+            label3.Text = "Oficio";
+            // 
+            // txtOficio
+            // 
+            txtOficio.Location = new Point(630, 56);
+            txtOficio.Name = "txtOficio";
+            txtOficio.Size = new Size(224, 39);
+            txtOficio.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(630, 108);
+            label4.Name = "label4";
+            label4.Size = new Size(85, 32);
+            label4.TabIndex = 6;
+            label4.Text = "Salario";
+            // 
+            // txtSalario
+            // 
+            txtSalario.Location = new Point(631, 142);
+            txtSalario.Name = "txtSalario";
+            txtSalario.Size = new Size(223, 39);
+            txtSalario.TabIndex = 7;
+            // 
+            // btnModificarDatos
+            // 
+            btnModificarDatos.Location = new Point(633, 206);
+            btnModificarDatos.Name = "btnModificarDatos";
+            btnModificarDatos.Size = new Size(221, 52);
+            btnModificarDatos.TabIndex = 8;
+            btnModificarDatos.Text = "Modificar datos";
+            btnModificarDatos.UseVisualStyleBackColor = true;
+            btnModificarDatos.Click += btnModificarDatos_Click;
+            // 
+            // Form05DepartamentosEmpleados
+            // 
+            AutoScaleDimensions = new SizeF(13F, 32F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(887, 400);
+            Controls.Add(btnModificarDatos);
+            Controls.Add(txtSalario);
+            Controls.Add(label4);
+            Controls.Add(txtOficio);
+            Controls.Add(label3);
+            Controls.Add(lstEmpleados);
+            Controls.Add(label2);
+            Controls.Add(lstDepartamentos);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(6);
+            Name = "Form05DepartamentosEmpleados";
+            Text = "Form05DepartamentosEmpleados";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox lstDepartamentos;
+        private Label label2;
+        private ListBox lstEmpleados;
+        private Label label3;
+        private TextBox txtOficio;
+        private Label label4;
+        private TextBox txtSalario;
+        private Button btnModificarDatos;
+    }
+}
---
+++ AdoNet/Form05DepartamentosEmpleados.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdoNet
+{
+    public partial class Form05DepartamentosEmpleados : Form
+    {
+        string connectionString;
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+
+        public Form05DepartamentosEmpleados()
+        {
+            InitializeComponent();
+            this.connectionString = @"Data Source=LOCALHOST\SQLEXPRESS;Initial Catalog=HOSPITAL;Persist Security Info=True;User ID=SA;Password=MCSD2023";
+            this.cn = new SqlConnection(this.connectionString);
+            this.com = new SqlCommand();
+            this.com.Connection = this.cn;
+            this.LoadDepartamentos();
+        }
+
+        private void LoadDepartamentos()
+        {
+            string sql = "select DNOMBRE from DEPT";
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            this.reader = this.com.ExecuteReader();
+            while (this.reader.Read())
+            {
+                this.lstDepartamentos.Items.Add(this.reader["DNOMBRE"].ToString());
+            }
+            this.reader.Close();
+            this.cn.Close();
+        }
+
+        private void lstDepartamentos_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string sql = "select EMP.APELLIDO from EMP " +
+                " inner join DEPT on EMP.DEPT_NO = DEPT.DEPT_NO "
+                + " where DEPT.DNOMBRE=@nombredepartamento";
+            string nombreDepartamento = this.lstDepartamentos.SelectedItem.ToString();
+            SqlParameter pamnombre =
+                new SqlParameter("@nombredepartamento", nombreDepartamento);
+            this.com.Parameters.Add(pamnombre);
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            this.reader = this.com.ExecuteReader();
+            this.lstEmpleados.Items.Clear();
+            while (this.reader.Read())
+            {
+                this.lstEmpleados.Items.Add(this.reader["APELLIDO"].ToString());
+            }
+            this.reader.Close();
+            this.com.Parameters.Clear();
+            this.cn.Close();
+        }
+
+        private void lstEmpleados_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string sql = "select OFICIO, SALARIO from EMP " +
+                " where APELLIDO=@apellido";
+            string apellido = this.lstEmpleados.SelectedItem.ToString();
+            SqlParameter pamapellido =
+                new SqlParameter("@apellido", apellido);
+            this.com.Parameters.Add(pamapellido);
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            this.reader = this.com.ExecuteReader();
+            this.reader.Read();
+            this.txtOficio.Text = this.reader["OFICIO"].ToString();
+            this.txtSalario.Text = this.reader["SALARIO"].ToString();
+            this.reader.Close();
+            this.com.Parameters.Clear();
+            this.cn.Close();
+        }
+
+        private void btnModificarDatos_Click(object sender, EventArgs e)
+        {
+            string sql = "update EMP set OFICIO=@oficio " 
+                + ", SALARIO=@salario " 
+                + " where APELLIDO=@apellido";
+            string oficio = this.txtOficio.Text;
+            int salario = int.Parse(this.txtSalario.Text);
+            string apellido = this.lstEmpleados.SelectedItem.ToString();
+            SqlParameter pamapellido =
+                new SqlParameter("@apellido", apellido);
+            SqlParameter pamoficio =
+                new SqlParameter("@oficio", oficio);
+            SqlParameter pamsalario =
+                new SqlParameter("@salario", salario);
+            this.com.Parameters.Add(pamsalario);
+            this.com.Parameters.Add(pamoficio);
+            this.com.Parameters.Add(pamapellido);
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            int updates = this.com.ExecuteNonQuery();
+            this.cn.Close();
+            this.com.Parameters.Clear();
+            MessageBox.Show("Modificados " + updates);
+        }
+    }
+}
